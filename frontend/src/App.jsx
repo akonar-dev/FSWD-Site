9b9d31874d8fdfd56cd35e6fc08b497f47bb392b
akonar-dev <157737602+akonar-dev@users.noreply.github.com>
2025-05-04 18:09:18 +0530

    login-page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
+<<<<<<< HEAD
 function App() {
 
   return (
     <>
     <h1>Bengal Coding Academy</h1>
     </>
+=======
+import { BrowserRouter, Routes, Route } from 'react-router'
+import Home from "./components/Home"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<Home/>} />
+        {/* Add more routes here as needed */}
+      </Routes>
+    </BrowserRouter>
+>>>>>>> e5808d8 (login-page)
   )
 }
 
